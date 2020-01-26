8b3bdeaf11fdf8863280cf5a1d619801151508d9
artsrunpetrosyan <55757882+artsrunpetrosyan@users.noreply.github.com>
2020-01-26 15:53:48 +0400

    Update regist.js

--- src/regist.js
+++ src/regist.js
@@ -4,31 +4,40 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const btn = document.getElementById("btn");
 
-btn.addEventListener("click", () => {
-    fetch(
-        "https://it-blog-posts.herokuapp.com/api/people",
-        {
-            method: "POST",
-            body: JSON.stringify({
-                lastname: `${lastName.value}`,
-                username: `${userName.value}`,
-                email: `${email.value}`,
-                password: `${password.value}`,
-            }),
-            headers: { "Content-Type": "application/json" }
-        }
-    )
-     .then(res => res.json())
-    .then(data => {
-        window.open("login.html")
-        return data;
-    })
-    .then(
-        window.open('log-in.html','_self')
-    )
-        .catch(function (err) {
-            console.log("Error: ", err)
-        });
 
+
+btn.addEventListener("click", () => {
+   
+    fetch( "https://it-blog-posts.herokuapp.com/api/people",
+    {
+        method: "POST",
+        body: JSON.stringify({
+                            lastname: `${lastName.value}`,
+                            username: `${userName.value}`,
+                            email: `${email.value}`,
+                            password: `${password.value}`,
+                        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(res => {
+        if(res.ok) {
+           
+            return res.json()
+        }           
+        
+    })
+    
+.then(data => {
+    window.open("log-in.html",'_self')
+    console.log(data)
+    return data;
 })
+    
+    .catch(function (err) {
+                    console.log("Error: ", err)
+                });
+})
+
 
